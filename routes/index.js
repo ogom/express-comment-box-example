72aa2303a62e37d023d6d2b45dcfcf8ec652c414
ogom <ogom@outlook.com>
2016-06-08 01:07:15 +0900

    Add comments read and write to router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var Remarkable = require('remarkable');
+var md = new Remarkable();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fs.readFile('db/comments.json', function(err, data) {
+    var comments = JSON.parse(data);
+    comments = comments.map(function(comment, index, array) {
+      return {author: comment.author, text: md.render(comment.text)};
+    });
+    res.render('index', {title: 'Comment Box Example', comments: comments});
+  });
+});
+
+/* POST home page. */
+router.post('/', function(req, res) {
+  fs.readFile('db/comments.json', function(err, data) {
+    var comments = JSON.parse(data);
+    req.body.id = Date.now();
+    comments.push(req.body);
+    fs.writeFile('db/comments.json', JSON.stringify(comments, null, 4), function(err) {
+      comments = comments.map(function(comment, index, array) {
+        return {author: comment.author, text: md.render(comment.text)};
+      });
+      res.render('index', {title: 'Comment Box Example', comments: comments});
+    });
+  });
 });
 
 module.exports = router;
